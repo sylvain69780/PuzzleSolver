363ad9027c845e6431994b0cb29e476ca36c2132
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-01-03 22:24:27 +0100

    start

---
+++ Algorithms/AdventOfCode/Y2023/Day20/PulsePropagation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.AdventOfCode.Y2023.Day20
+{
+    public class PulsePropagation
+    {
+        public IEnumerable<string> Solve(string input, string part)
+        {
+            var algo = _parts[part];
+            var data = Parse(input);
+            return algo(data);
+        }
+
+        StringDataModel Parse(string input)
+        {
+            return new StringDataModel()
+            {
+                DataToProcess = input,
+            };
+        }
+
+        static Dictionary<string, Func<StringDataModel, IEnumerable<string>>> _parts = new()
+        {
+            { "Part 1" ,PartOne },
+            { "Part 2", PartTwo }
+        };
+
+        public IEnumerable<string> Strategies => _parts.Keys;
+
+        static IEnumerable<string> PartOne(StringDataModel input)
+        {
+            yield return input.DataToProcess;
+        }
+        static IEnumerable<string> PartTwo(StringDataModel input)
+        {
+            yield return input.DataToProcess;
+        }
+
+
+    }
+}
---
+++ PuzzleSolverTests/AdventOfCode/Y2023/Day20/PulsePropagationTests.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.AdventOfCode.Y2023.Day20;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverTests.AdventOfCode.Y2023.Day20
+{
+    public class PulsePropagationTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var solver = new PulsePropagation();
+            Assert.Equal("32000000", solver.Solve(input, "Part 1").Last());
+        }
+
+        const string path = "..\\..\\..\\..\\PuzzleSolver\\wwwroot\\sample-data\\AdventOfCode\\Y2023\\Day20";
+        string input => File.ReadAllText($"{path}\\PulsePropagation.txt");
+
+        string input2 => File.ReadAllText($"{path}\\PulsePropagation_full.txt");
+    }
+}
