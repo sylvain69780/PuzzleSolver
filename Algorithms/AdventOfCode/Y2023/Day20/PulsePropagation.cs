22d633f0793ed68e6f85bf91339394553ae7bc36
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-01-14 19:34:43 +0100

    20 part 1

--- Algorithms/AdventOfCode/Y2023/Day20/PulsePropagation.cs
+++ Algorithms/AdventOfCode/Y2023/Day20/PulsePropagation.cs
@@ -21,7 +21,7 @@ namespace Algorithms.AdventOfCode.Y2023.Day20
             var broadcaster = new string[] { "broadcaster" };
             return new PulsePropagationDataModel()
             {
-         
+
                 ModuleConfiguration = input.Split("\n")
                             .Select(x => x.Split(" -> "))
                             .Select(x => (name: x[0], destinations: x[1].Split(", ")))
@@ -30,7 +30,7 @@ namespace Algorithms.AdventOfCode.Y2023.Day20
                                 if (x.name[0] == '&' || x.name[0] == '%')
                                     return (type: x.name[0], name: x.name[1..], x.destinations);
                                 else
-                                    return (type:' ',x.name,x.destinations);
+                                    return (type: ' ', x.name, x.destinations);
                             })
                             .Append((' ', "button", broadcaster))
                             .Append((' ', "output", Array.Empty<string>()))
@@ -48,43 +48,63 @@ namespace Algorithms.AdventOfCode.Y2023.Day20
 
         static IEnumerable<string> PartOne(PulsePropagationDataModel input)
         {
-            var c = input.ModuleConfiguration!;
-            var flipFlops = c
+            var configuraton = input.ModuleConfiguration!;
+            var flipFlops = configuraton
                 .Where(x => x.type == '%')
-                .ToDictionary(x =>x.name,x=> "off");
-            var conjonctions = c
+                .ToDictionary(x => x.name, x => "off");
+            var conjonctions = configuraton
                 .Where(x => x.type == '&')
-                .Select(x => (x.name,connected:c.Where(y => y.destinations.Contains(x.name)).Select(y => (y.name,lastPulse:"-low")).ToArray()))
-                .ToDictionary(x=>x.name,x=>x.connected);
-            var types = c.ToDictionary(x => x.name, x => x.type);
-            var destinations = c.ToDictionary(x => x.name, x => x.destinations);
-
-            var bfs = new Queue<(string name, string pulse)>();
-            bfs.Enqueue(("button", "-low"));
-            while ( bfs.Count>0)
+                .Select(x => (x.name, connected: configuraton.Where(y => y.destinations.Contains(x.name)).Select(y => (y.name, lastPulse: "-low")).ToDictionary(x => x.name, x => x.lastPulse)))
+                .ToDictionary(x => x.name, x => x.connected);
+            var types = configuraton.ToDictionary(x => x.name, x => x.type);
+            var destinations = configuraton.ToDictionary(x => x.name, x => x.destinations);
+            var high = 0;
+            var low = 0;
+            for (var i = 0; i < 1000; i++)
             {
-                var newQueue = new Queue<(string name, string pulse)>();
-                while(bfs.TryDequeue(out var pulse))
+                var bfs = new Queue<(string name, string pulse, string pulseOrigin)>();
+                bfs.Enqueue(("button", "-low", string.Empty));
+                while (bfs.Count > 0)
                 {
-                    var type = types[pulse.name];
-                    if (type == ' ')
+                    var newQueue = new Queue<(string name, string pulse, string pulseOrigin)>();
+                    while (bfs.TryDequeue(out var module))
                     {
-                        foreach (var item in destinations[pulse.name])
-                            newQueue.Enqueue((item, pulse.pulse));
+                        if (module.name != "button")
+                            if (module.pulse == "-high")
+                                high++;
+                            else
+                                low++;
+                        if (!types.TryGetValue(module.name, out var type))
+                            continue;
+                        if (type == ' ')
+                        {
+                            foreach (var item in destinations[module.name])
+                                newQueue.Enqueue((item, module.pulse, module.name));
+                        }
+                        if (type == '%')
+                        {
+                            if (module.pulse == "-high")
+                                continue;
+                            var v = flipFlops[module.name];
+                            flipFlops[module.name] = v == "on" ? "off" : "on";
+                            var p = v == "on" ? "-low" : "-high";
+                            foreach (var item in destinations[module.name])
+                                newQueue.Enqueue((item, p, module.name));
+                        }
+                        if (type == '&')
+                        {
+                            var conjModule = conjonctions[module.name];
+                            conjModule[module.pulseOrigin] = module.pulse;
+                            var pulse = conjModule.All(x => x.Value == "-high") ? "-low" : "-high";
+                            foreach (var targetModule in destinations[module.name])
+                                newQueue.Enqueue((targetModule, pulse, module.name));
+                        }
                     }
-                    if (type == '%' && pulse.pulse == "-low")
-                    {
-                        var v = flipFlops[pulse.name];
-                        flipFlops[pulse.name] = v == "on" ? "off" : "on";
-                        var p = v == "on" ? "-low" : "-high";
-                        foreach (var item in destinations[pulse.name])
-                            newQueue.Enqueue((item, p));
-                    }
+                    bfs = newQueue;
                 }
-                bfs = newQueue;
             }
 
-            yield return string.Empty;
+            yield return (high * low).ToString();
         }
         static IEnumerable<string> PartTwo(PulsePropagationDataModel input)
         {
--- PuzzleSolverTests/AdventOfCode/Y2023/Day20/PulsePropagationTests.cs
+++ PuzzleSolverTests/AdventOfCode/Y2023/Day20/PulsePropagationTests.cs
@@ -15,6 +15,7 @@ namespace PuzzleSolverTests.AdventOfCode.Y2023.Day20
             var solver = new PulsePropagation();
             Assert.Equal("32000000", solver.Solve(input, "Part 1").Last());
             Assert.Equal("11687500", solver.Solve(input1, "Part 1").Last());
+            Assert.Equal("818649769", solver.Solve(input2, "Part 1").Last());
         }
 
         const string path = "..\\..\\..\\..\\PuzzleSolver\\wwwroot\\sample-data\\AdventOfCode\\Y2023\\Day20";
