139ae1072c462e0e8187cfdf8ea3e69ca327eb59
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-02-05 12:53:03 +0100

    day 23

--- Algorithms/AdventOfCode/Y2023/Day23/ALongWalk.cs
+++ Algorithms/AdventOfCode/Y2023/Day23/ALongWalk.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
@@ -34,9 +35,9 @@ namespace Algorithms.AdventOfCode.Y2023.Day23
 
         static readonly char[] TilesOk = ['.', '>', '<', '^', 'v'];
 
-        static bool IsInpossible(char tile,(int x, int y) dir)
+        static bool IsInpossible(char tile, (int x, int y) dir)
         {
-            return 
+            return
                 (tile == '>' && dir == (-1, 0)) ||
                 (tile == '<' && dir == (1, 0)) ||
                 (tile == '^' && dir == (0, 1)) ||
@@ -124,68 +125,285 @@ namespace Algorithms.AdventOfCode.Y2023.Day23
             yield return (results.Max() - 1).ToString();
         }
 
+        // https://www.reddit.com/r/adventofcode/comments/18oy4pc/comment/kfyvp2g/
+
         public IEnumerable<string> PartTwo(ALongWalkDataModel model)
+        {
+            var bfsMap = model.Map!.Select(l => l.ToArray()).ToArray();
+            var start = (x: 1, y: 0);
+            var exit = (x: bfsMap[0].Length - 2, y: bfsMap.Length - 1);
+            var nodes = new HashSet<(int x, int y)>();
+            nodes.Add(start);
+            nodes.Add(exit);
+            for (var y = 1; y < exit.y; y++)
+                for (var x = 1; x <= exit.x; x++)
+                {
+                    var tile2 = bfsMap[y][x];
+                    if (tile2 == '#')
+                        continue;
+                    var forks = 0;
+                    foreach (var dir in directions)
+                    {
+                        var pos = (x: dir.x + x, y: dir.y + y);
+                        var tile = bfsMap[pos.y][pos.x];
+                        if (tile == '#')
+                            continue;
+                        forks++;
+                    }
+                    if (forks > 2)
+                    {
+                        bfsMap[y][x] = 'F';
+                        nodes.Add((x, y));
+                    }
+                }
+            // TO DO missing 19,13   5,13 - exploration en meme temps du meme segment.
+            var bfsMap2 = bfsMap.Select(x => x.ToArray()).ToArray();
+
+            var distances = new Dictionary<((int x, int y) p1, (int x, int y) p2), int>();
+
+            foreach (var node in nodes)
+            {
+                foreach (var dir in directions)
+                {
+                    var pos = (x: dir.x + node.x, y: dir.y + node.y);
+                    if (pos.y <= 0 || pos.y >= exit.y) continue;
+                    var tile = bfsMap[pos.y][pos.x];
+                    if (tile != '#')
+                    {
+                        var distance = 1;
+                        var prev = node;
+                        while (!nodes.Contains(pos))
+                        {
+                            distance++;
+                            foreach (var dir2 in directions)
+                            {
+                                var npos = (x: dir2.x + pos.x, y: dir2.y + pos.y);
+                                if (npos == prev)
+                                    continue;
+                                var tile2 = bfsMap[npos.y][npos.x];
+                                if (tile2 != '#')
+                                {
+                                    prev = pos;
+                                    pos = npos;
+                                    break;
+                                }
+                            }
+                        }
+                        distances.Add((node, pos), distance);
+                    }
+                }
+
+            }
+
+            //var bfs = new Queue<((int x, int y) start, (int x, int y) pos, int distance)>();
+            //bfsMap[0][1] = '#';
+            //bfs.Enqueue((start,start, 0));
+            //while (bfs.TryDequeue(out var item))
+            //{
+            //    var (orig, p, distance) = item;
+            //    if (p == exit)
+            //        continue;
+            //    bfsMap[p.y][p.x] = '#';
+            //    foreach (var dir in directions)
+            //    {
+            //        var pos = (x: dir.x + p.x, y: dir.y + p.y);
+            //        var d = distance+1;
+            //        if (pos.y <= 0) continue;
+            //        if (orig != pos && nodes.Contains(pos))
+            //        {
+            //            distances.TryAdd((orig, pos), d);
+            //            orig = pos;
+            //            d = 0;
+            //        }
+            //        var tile = bfsMap[pos.y][pos.x];
+            //        if (tile != '#')
+            //            bfs.Enqueue((orig, pos, d));
+            //    }
+            //}
+            //Visu2(bfsMap);
+            //foreach(var n in nodes)
+            //{
+            //    bfsMap2[n.y][n.x] = 'X';
+            //}
+
+            var longestDistance = 0;
+            var bestPath = new List<(int x, int y)>();
+            var dfs = new Stack<((int x, int y) p, int d)>();
+            var explored = new HashSet<(int x, int y)>();
+            dfs.Push((start, 0));
+
+            var distDico = distances.GroupBy(d => d.Key.p1)
+                .Select(g => (g.Key, neighbourgs: g.Select(p => (neighbourg: p.Key.p2, d: p.Value)).ToList()))
+                .ToDictionary(g => g.Key, g => g.neighbourgs);
+            while (dfs.TryPop(out var item))
+            {
+                var (pos, distance) = item;
+                if (explored.Contains(pos))
+                {
+                    explored.Remove(pos);
+                    continue;
+                }
+                else
+                {
+                    dfs.Push(item);
+                    explored.Add(pos);
+                }
+                var neighbors = distDico[pos]
+                    .Where(n => !explored.Contains(n.neighbourg));
+                foreach (var n in neighbors)
+                {
+                    var (npos, d) = n;
+                    d += distance;
+                    if (npos == exit)
+                    {
+                        if (d > longestDistance)
+                        {
+                            longestDistance = d;
+                            bestPath = explored.ToList();
+                        }
+
+                        continue;
+                    }
+                    dfs.Push((npos, d));
+                }
+            }
+
+            yield return longestDistance.ToString();
+        }
+
+
+        public IEnumerable<string> PartTwoNaive2(ALongWalkDataModel model)
         {
             var map = model.Map!;
             var start = (x: 1, y: 0);
             var exit = (x: map[0].Length - 2, y: map.Length - 1);
             var hash = new HashSet<(int x, int y)>();
             hash.Add(start);
-            var bfs = new Queue<(HashSet<(int x, int y)> hash,(int x, int y) pos)>();
-            bfs.Enqueue((hash,start));
+            var dfs = new Stack<(HashSet<(int x, int y)> hash, (int x, int y) pos)>();
+            dfs.Push((hash, start));
             var results = new List<int>();
             var best = new List<(int x, int y)>();
-            while (bfs.Count > 0)
+            while (dfs.TryPop(out var rec))
             {
-                var newQueue = new Queue<(HashSet<(int x, int y)> hash, (int x, int y) pos)>();
-                while (bfs.TryDequeue(out var rec))
+                var head = rec.pos;
+                var forks = new List<(int x, int y)>();
+                foreach (var dir in directions)
                 {
-                    var head = rec.pos;
-                    var forks = new List<(int x, int y)>();
-                    foreach (var dir in directions)
+                    if (head.x == 1 && dir == (0, -1))
+                        continue;
+                    if (head.x == map[0].Length - 2 && dir == (0, -1))
+                        continue;
+                    if (head.y == 1 && dir == (-1, 0))
+                        continue;
+                    if (head.y == map.Length - 2 && dir == (-1, 0))
+                        continue;
+                    var pos = (x: dir.x + head.x, y: dir.y + head.y);
+                    var tile = Tile(map, pos);
+                    if (tile == '#')
+                        continue;
+                    if (pos == exit)
                     {
-                        var pos = (x: dir.x + head.x, y: dir.y + head.y);
-                        var tile = Tile(map, pos);
-                        if (tile == '#')
-                            continue;
-                        if (pos == exit)
+                        rec.hash.Add(pos);
+                        if (rec.hash.Count > best.Count)
                         {
-                            rec.hash.Add(pos);
                             results.Add(rec.hash.Count);
-                            continue;
+                            best = [.. rec.hash];
                         }
-                        if (TilesOk.Contains(tile) && !rec.hash.Contains(pos))
-                        {
-                            forks.Add(pos);
-                        }
+                        //Visu(best, map);
+                        continue;
                     }
-                    if (forks.Count > 1)
-                        forks[1..].ForEach(p =>
-                        {
-                            var newHash = rec.hash.ToHashSet();
-                            newHash.Add(p);
-                            newQueue.Enqueue((newHash,p));
-                        });
-                    if (forks.Count > 0)
+                    if (TilesOk.Contains(tile) && !rec.hash.Contains(pos))
                     {
-                        var p = forks[0];
-                        rec.hash.Add(p);
-                        newQueue.Enqueue((rec.hash,p));
+                        forks.Add(pos);
                     }
                 }
-                bfs = newQueue;
+                if (forks.Count > 1)
+                    forks[1..].ForEach(p =>
+                    {
+                        var newHash = rec.hash.ToHashSet();
+                        newHash.Add(p);
+                        dfs.Push((newHash, p));
+                    });
+                if (forks.Count > 0)
+                {
+                    var p = forks[0];
+                    rec.hash.Add(p);
+                    dfs.Push((rec.hash, p));
+                }
             }
-            foreach (var p in best)
+            yield return (results.Max() - 1).ToString();
+        }
+        public IEnumerable<string> PartTwoNaive(ALongWalkDataModel model)
+        {
+            var map = model.Map!;
+            var start = (x: 1, y: 0);
+            var exit = (x: map[0].Length - 2, y: map.Length - 1);
+            var hash = new HashSet<(int x, int y)>();
+            hash.Add(start);
+            var dfs = new Stack<(HashSet<(int x, int y)> hash, (int x, int y) pos)>();
+            dfs.Push((hash, start));
+            var results = new List<int>();
+            var best = new List<(int x, int y)>();
+            while (dfs.TryPop(out var rec))
             {
-                var line = new StringBuilder(map[p.y]);
-                line[p.x] = 'O';
-                map[p.y] = line.ToString();
-                var test = string.Join('\n', map);
+                var head = rec.pos;
+                var forks = new List<(int x, int y)>();
+                foreach (var dir in directions)
+                {
+                    var pos = (x: dir.x + head.x, y: dir.y + head.y);
+                    var tile = Tile(map, pos);
+                    if (tile == '#')
+                        continue;
+                    if (pos == exit)
+                    {
+                        rec.hash.Add(pos);
+                        if (rec.hash.Count > best.Count)
+                        {
+                            results.Add(rec.hash.Count);
+                            best = [.. rec.hash];
+                        }
+                        //Visu(best, map);
+                        continue;
+                    }
+                    if (TilesOk.Contains(tile) && !rec.hash.Contains(pos))
+                    {
+                        forks.Add(pos);
+                    }
+                }
+                if (forks.Count > 1)
+                    forks[1..].ForEach(p =>
+                    {
+                        var newHash = rec.hash.ToHashSet();
+                        newHash.Add(p);
+                        dfs.Push((newHash, p));
+                    });
+                if (forks.Count > 0)
+                {
+                    var p = forks[0];
+                    rec.hash.Add(p);
+                    dfs.Push((rec.hash, p));
+                }
             }
             yield return (results.Max() - 1).ToString();
         }
 
+        static void Visu(List<(int x, int y)> best, string[] map)
+        {
+            var mapCopy = map.ToArray();
+            foreach (var p in best)
+            {
+                var line = new StringBuilder(mapCopy[p.y]);
+                line[p.x] = 'O';
+                mapCopy[p.y] = line.ToString();
+                //var test = string.Join('\n', mapCopy);
+            }
+            var test = string.Join('\n', mapCopy);
+        }
 
+        static void Visu2(char[][] map)
+        {
+            var test = string.Join('\n', map.Select(l => new string(l)));
+        }
     }
 }
 
--- PuzzleSolverTests/AdventOfCode/Y2023/Day23/ALongWalkTests.cs
+++ PuzzleSolverTests/AdventOfCode/Y2023/Day23/ALongWalkTests.cs
@@ -16,7 +16,7 @@ namespace PuzzleSolverTests.AdventOfCode.Y2023.Day23
         {
             var solver = new ALongWalk();
             Assert.Equal("154", solver.Solve(input, "Part 2").Last());
-            Assert.Equal("102770", solver.Solve(input2, "Part 2").Last());
+            Assert.Equal("6654", solver.Solve(input2, "Part 2").Last());
         }
 
         const string path = "..\\..\\..\\..\\PuzzleSolver\\wwwroot\\sample-data\\AdventOfCode\\Y2023\\Day23";
