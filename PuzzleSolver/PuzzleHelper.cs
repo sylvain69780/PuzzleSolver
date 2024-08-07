94ddb2bdde77414570476debba2c32b5ce368e3c
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-08-07 20:23:56 +0200

    ok

---
+++ PuzzleSolver/PuzzleHelper.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.AdventOfCode;
+using System.Reflection;
+
+namespace PuzzleSolver
+{
+    public static class PuzzleHelper
+    {
+        public static string GetPuzzleTitle(Type type) => ((SolutionAttribute)Attribute.GetCustomAttribute(type, typeof(SolutionAttribute))!).Description;
+
+        public static List<(string Name, Func<TInput, IEnumerable<TState>> Method)> GetPuzzleMethods<TInput, TState>(Type type)
+        {
+            MethodInfo[] methodsWithAttribute = type.GetMethods()
+                .Where(method => Attribute.IsDefined(method, typeof(SolutionMethodAttribute)))
+                .ToArray();
+            var methods = new List<(string Name, Func<TInput, IEnumerable<TState>> Method)>();
+            foreach (var method in methodsWithAttribute)
+            {
+                var customAttribute = (SolutionMethodAttribute)Attribute.GetCustomAttribute(method, typeof(SolutionMethodAttribute))!;
+                methods.Add((customAttribute.Description, (Func<TInput, IEnumerable<TState>>)method.CreateDelegate(typeof(Func<TInput, IEnumerable<TState>>))));
+            }
+            return methods;
+        }
+    }
+}
