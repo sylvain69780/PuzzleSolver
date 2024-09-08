bcccc93159bff5e1053f1a683fa2443ae897d0cf
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-09-08 13:21:01 +0200

    ho

--- PuzzleSolver/Pages/AdventOfCode/Y2023/Day20/Page.razor.cs
+++ PuzzleSolver/Pages/AdventOfCode/Y2023/Day20/Page.razor.cs
@@ -6,11 +6,7 @@ namespace PuzzleSolver.Pages.AdventOfCode.Y2023.Day20
 {
     public partial class Page : ComponentBase
     {
-        private int delay = 1;
-        private Input? input;
         private List<ISolutionFinder<Input>> solutionFinders = [new SolutionFinder1(), new SolutionFinder2()];
-        private int selectedSolution = 0;
-        private ISolutionFinder<Input> selectedSolutionFinder => solutionFinders[selectedSolution];
         private KeyValuePair<string, string>[] files = [
             new KeyValuePair<string,string>("Example","sample-data/AdventOfCode/Y2023/Day20/PulsePropagation.txt"),
             new KeyValuePair<string,string>("Example2","sample-data/AdventOfCode/Y2023/Day20/PulsePropagation2.txt"),
---
+++ PuzzleSolver/SolutionFindersCollection.razor.cs
@@ -0,0 +1,21 @@
+﻿using Algorithms;
+using Microsoft.AspNetCore.Components;
+
+namespace PuzzleSolver
+{
+    public partial class SolutionFindersCollection<TInput> : ComponentBase
+    {
+        [Parameter]
+        public List<ISolutionFinder<TInput>>? SolutionFinders { get; set; }
+        [Parameter]
+        public KeyValuePair<string, string>[]? InputFiles { get; set; }
+        [Parameter]
+        public Func<string,TInput>? Parse { get; set; }
+
+        private int selectedSolution = 0;
+        private int delay = 1;
+
+        private TInput? input;
+        private ISolutionFinder<TInput> SelectedSolutionFinder => SolutionFinders![selectedSolution];
+    }
+}
