3d56e1f3bbb1d7f3cdac1d63ee5e72bf747d50cc
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-09-08 10:03:02 +0200

    ok

---
+++ PuzzleSolver/Pages/AdventOfCode/Y2023/Day20/Page.razor.cs
@@ -0,0 +1,21 @@
+﻿using Algorithms.AdventOfCode.Y2023.Day20;
+using Algorithms;
+using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+
+namespace PuzzleSolver.Pages.AdventOfCode.Y2023.Day20
+{
+    public partial class Page : ComponentBase
+    {
+        private int delay = 1;
+        private Input? input;
+        private List<ISolutionFinder<Input>> solutionFinders = [new SolutionFinder1(), new SolutionFinder2()];
+        private int selectedSolution = 0;
+        private ISolutionFinder<Input> selectedSolutionFinder => solutionFinders[selectedSolution];
+        private KeyValuePair<string, string>[] files = [
+            new KeyValuePair<string,string>("Example","sample-data/AdventOfCode/Y2023/Day20/PulsePropagation.txt"),
+            new KeyValuePair<string,string>("Example2","sample-data/AdventOfCode/Y2023/Day20/PulsePropagation2.txt"),
+            new KeyValuePair<string,string>("Full","sample-data/AdventOfCode/Y2023/Day20/PulsePropagation_full.txt")
+        ];
+    }
+}
